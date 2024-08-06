22a93b3cd5ea0c4efd5fc18362f88c328f26bd91
Xinyang Wang <mrxywang@gmail.com>
2024-08-05 23:38:51 -0400

    Basic draw to screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
+#include <cstdlib>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world and hello arcade :D" << std::endl;
-    return 0;
+    using namespace ftxui;
+
+    Element doc = hbox({
+        text("left") | border,
+        text("middle") | border | flex,
+        text("right") | border,
+    });
+
+    auto screen = Screen::Create(Dimension::Full(), // width
+                                 Dimension::Fit(doc));
+
+    Render(screen, doc);
+    screen.Print();
+
+    return EXIT_SUCCESS;
 }
