e6fb7a37b712965828c84fd0b5351d80c22c3a34
Xinyang Wang <mrxywang@gmail.com>
2024-08-05 23:49:57 -0400

    Added TODO box

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,30 @@
 #include <cstdlib>
 #include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/color.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <iostream>
 
 int main() {
     using namespace ftxui;
 
-    /*Element doc = hbox({*/
-    /*    text("TODO") | border,*/
-    /*    text("Tree") | border | flex,*/
-    /*});*/
-
-    /*auto screen = Screen::Create(Dimension::Full(), // width*/
-    /*                             Dimension::Fit(doc));*/
-
-    /*Render(screen, doc);*/
-    /*screen.Print();*/
-
     auto screen = Screen::Create(Dimension::Fixed(32), Dimension::Fixed(16));
+    Element todo = vbox({text("TODO List:") | bold | color(Color::Blue),
+                         gauge(6), text("The end")});
+
+    // docs https://arthursonzogni.github.io/FTXUI/
+    // so much documentation
+
+    todo = border(todo);
+    Render(screen, todo);
+    screen.Print();
+
+    /*text("Welcome to your new TODO app :D");*/
+    screen.Print();
+
     auto& pixel = screen.PixelAt(9, 9);
     pixel.character = U'A';
     pixel.bold = true;
     pixel.foreground_color = Color::Blue;
-
-    std::cout << screen.ToString() << std::endl;
 
     return EXIT_SUCCESS;
 }
