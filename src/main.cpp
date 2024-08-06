2c6baf1ccc9fe4252ffe7312a81d144d6a48fc51
Xinyang Wang <mrxywang@gmail.com>
2024-08-05 23:44:17 -0400

    Text positioning

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,24 @@
 int main() {
     using namespace ftxui;
 
-    Element doc = hbox({
-        text("left") | border,
-        text("middle") | border | flex,
-        text("right") | border,
-    });
+    /*Element doc = hbox({*/
+    /*    text("TODO") | border,*/
+    /*    text("Tree") | border | flex,*/
+    /*});*/
 
-    auto screen = Screen::Create(Dimension::Full(), // width
-                                 Dimension::Fit(doc));
+    /*auto screen = Screen::Create(Dimension::Full(), // width*/
+    /*                             Dimension::Fit(doc));*/
 
-    Render(screen, doc);
-    screen.Print();
+    /*Render(screen, doc);*/
+    /*screen.Print();*/
+
+    auto screen = Screen::Create(Dimension::Fixed(32), Dimension::Fixed(16));
+    auto& pixel = screen.PixelAt(9, 9);
+    pixel.character = U'A';
+    pixel.bold = true;
+    pixel.foreground_color = Color::Blue;
+
+    std::cout << screen.ToString() << std::endl;
 
     return EXIT_SUCCESS;
 }
